791c047ce0673cdc4f438afd3fbdb32ac3d22021
SuryanshRajawat <suryanshrajawat.cs19@gmail.com>
2026-02-06 17:23:52 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BMICalculator from './BMICalculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BMICalculator />
   </React.StrictMode>
 );
 
